11f8381a97c1221ea1c4928e0922e55b457cc23e
rohandhanraj <rdy5674@gmail.com>
2025-05-21 15:30:27 +0530

    run and render

--- run.sh
+++ run.sh
@@ -6,8 +6,8 @@ pip install -r requirements.txt
 echo "📦 Installing Node.js dependencies..."
 npm install
 
-echo "📦 Installing Playwright Linux system dependencies (user-space)..."
-npx playwright install-deps
+#echo "📦 Installing Playwright Linux system dependencies (user-space)..."
+#npx playwright install-deps
 
 echo "🌐 Installing Playwright browsers..."
 npx playwright install
