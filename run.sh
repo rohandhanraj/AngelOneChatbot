e2a30147da12d8485e1e3ee92fb85c8fd35a7d50
rohandhanraj <rdy5674@gmail.com>
2025-05-21 11:40:58 +0530

    added run render

--- run.sh
+++ run.sh
@@ -3,6 +3,3 @@ pip install -r requirements.txt
 
 echo "🌐 Installing Playwright and required browsers..."
 playwright install --with-deps
-
-echo "🚀 Launching Streamlit app..."
-streamlit run app.py --server.port=10000
